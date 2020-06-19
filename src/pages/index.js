609d690fb3bf1e5c8400e22521324fb154c379c5
MagalieLindaChetrit <magaliechetrit@gmail.com>
2020-06-19 17:58:53 +0200

    change to contact

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,6 @@ const IndexPage = () => {
                 <Head title="Home"/>
                 <h1>Hello.</h1>
                 <h2>I'm Maggie, a full-stack developer living in beautiful Arnhem.</h2>
-                <p>Need a developer? <a href="/contact">Contact me</a></p>
                 <p>Need a developer? <Link to="/contact">Contact me.</Link></p>
             </Layout>
 
